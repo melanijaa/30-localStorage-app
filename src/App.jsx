90897bf8b35356040310b473f84d274e1d77d9da
Melanija <melanija.sibakovska1@gmail.com>
2022-06-10 12:25:54 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,76 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./bootstrap.css";
+import "./crud.scss";
+import Create from "./Components/Create";
+import List from "./Components/List";
+import { create, edit, read, remove } from "./Functions/localStorage";
+import Edit from "./Components/Edit";
 
 function App() {
+  const [lastUpdate, setLastUpdate] = useState(Date.now());
+
+  const [exes, setExes] = useState(null);
+  const [modalData, setModalData] = useState(null);
+
+  const [createData, setCreateData] = useState(null);
+  const [deleteData, setDeleteData] = useState(null);
+  const [editData, setEditData] = useState(null);
+
+  //Read
+  useEffect(() => {
+    setExes(read());
+  }, [lastUpdate]);
+
+  // Create
+  useEffect(() => {
+    if (null === createData) {
+      return;
+    }
+    create(createData);
+    setLastUpdate(Date.now());
+  }, [createData]);
+
+  // Delete
+  useEffect(() => {
+    if (null === deleteData) {
+      return;
+    }
+    remove(deleteData);
+    setLastUpdate(Date.now());
+  }, [deleteData]);
+
+  // Edit
+  useEffect(() => {
+    if (null === editData) {
+      return;
+    }
+    edit(editData);
+    setLastUpdate(Date.now());
+  }, [editData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <Create setCreateData={setCreateData}></Create>
+          </div>
+          <div className="col-8">
+            <List
+              exes={exes}
+              setDeleteData={setDeleteData}
+              setModalData={setModalData}
+            ></List>
+          </div>
+        </div>
+      </div>
+      <Edit
+        setEditData={setEditData}
+        modalData={modalData}
+        setModalData={setModalData}
+      ></Edit>
+    </>
   );
 }
-
 export default App;
+
---
+++ src/Components/List.jsx
@@ -0,0 +1,29 @@
+// import { useState } from "react";
+
+import Ex from "./Ex";
+
+function List({ exes, setDeleteData, setModalData }) {
+  return (
+    <div className="card mt-4">
+      <div className="card-header">
+        <h2>List of Ex`es</h2>
+      </div>
+      <div className="card-body">
+        <ul className="list-group">
+          {exes
+            ? exes.map((ex) => (
+                <Ex
+                  key={ex.id}
+                  ex={ex}
+                  setDeleteData={setDeleteData}
+                  setModalData={setModalData}
+                ></Ex>
+              ))
+            : null}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default List;
