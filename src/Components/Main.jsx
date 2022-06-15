1e10d7fe366e36d230465aaac44b744c70d668c1
Melanija <melanija.sibakovska1@gmail.com>
2022-06-15 19:12:30 +0300

    css

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,15 +1,29 @@
 import kolt from "../images/kolt.png";
+import appStore from "../images/en.svg";
+import googlePlay from "../images/googlePlay.svg";
 
 const Main = () => {
-    return (
-      <div className="main">
-        <div className="text">
-        <h1>Safe.<br/>Affordable.<br/>Eco-friendly.</h1>
-        <p>Use Kolt scooters for your daily commute and to explore cities around the world.</p>
-        </div>
-        <img className="kolt" src={kolt} alt="kolt"></img>
+  return (
+    <div className="main">
+      <div className="text">
+        <h1>
+          Safe.
+          <br />
+          Affordable.
+          <br />
+          Eco-friendly.
+        </h1>
+        <p>
+          Use Kolt scooters for your daily commute and to explore cities around
+          the world.
+        </p>
+        <a className="appStore" href="https://example.com/">
+          <img src={appStore} alt="appStore"></img>
+        </a>
       </div>
-    );
-  };
-  
-  export default Main;
+      <img className="kolt" src={kolt} alt="kolt"></img>
+    </div>
+  );
+};
+
+export default Main;
